05dcaee54f1e85043219734ef087397f18f8bb27
Mirjamol <mirjamolq828@gmail.com>
2024-12-18 17:56:12 +0500

    Initial commit

---
+++ build-logic/conventions/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "wallpaper.app.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+gradlePlugin{
+    plugins{
+
+    }
+}
---
+++ build-logic/settings.gradle.kts
@@ -0,0 +1,19 @@
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+}
+
+rootProject.name = "build-logic"
+include(":convention")
