63c2e0a1aa7fd8e39759637fa4302f0822cc90ba
Mirjamol <mirjamolq828@gmail.com>
2025-03-13 15:07:34 +0500

    non necessary codes removed

--- core/core-ui/src/main/kotlin/com/example/nativetemplate/core_ui/theme/Shapes.kt
+++ core/core-ui/src/main/kotlin/com/example/nativetemplate/core_ui/theme/Shapes.kt
@@ -7,6 +7,4 @@ object ApplicationShapes{
     val Rounded12 = RoundedCornerShape(12.dp)
     val Rounded10 = RoundedCornerShape(10.dp)
     val Rounded8 = RoundedCornerShape(8.dp)
-    val bottomSheetShape: Shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
-    val Rounded2: Shape = RoundedCornerShape(2.dp)
 }
